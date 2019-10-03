d3029daf334ca50dfd5d4002d08546d3e6d49623
gdurbin <gerrettdurbin@gmail.com>
2019-10-03 12:08:15 -0700

    removed php for contact form

--- dist/scripts/script.js
+++
@@ -1,4 +0,0 @@
-var frmvalidator = new Validator(“contact_form”);
-frmvalidator.addValidation(“name”,”req”,”Please provide your name”);
-frmvalidator.addValidation(“email”,”req”,”Please provide your email”);
-frmvalidator.addValidation(“email”,”email”, “Please enter a valid email address”);
